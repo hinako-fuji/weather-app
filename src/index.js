b5526c281658a1cb992f20a0ab75f4d2e9242b58
hinako-fuji <117415389+hinako-fuji@users.noreply.github.com>
2022-11-06 09:45:32 +0900

    main icon fetched from api

--- src/index.js
+++ src/index.js
@@ -36,6 +36,12 @@ function showWeather(response) {
   document.querySelector("#day-time").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
